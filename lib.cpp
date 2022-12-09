7de58cdc6bf55c0f0a64aef9ccb5b6292fb21d2c
lorenzoluzzi <117952492+lorenzoluzzi@users.noreply.github.com>
2022-12-09 11:01:34 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+bool riconoscere(char o){
+    if (o=='a' || o=='b'|| o=='c'|| o=='d'|| o=='e'|| o=='f'|| o=='g'|| o=='h'|| o=='i'|| o=='j'|| o=='k'|| o=='l'|| o=='m'|| o=='n'|| o=='o'|| o=='p'|| o=='q'|| o=='r'|| o=='s'|| o=='t'|| o=='u'|| o=='v'|| o=='w'|| o=='x'|| o=='y'|| o=='z'){
+        return 0;
+    }
+    if (o=='A' || o=='B'|| o=='C'|| o=='D'|| o=='E'|| o=='F'|| o=='G'|| o=='H'|| o=='I'|| o=='J'|| o=='K'|| o=='L'|| o=='M'|| o=='N'|| o=='O'|| o=='P'|| o=='Q'|| o=='R'|| o=='S'|| o=='T'|| o=='U'|| o=='V'|| o=='W'|| o=='X'|| o=='Y'|| o=='Z'){
+        return 0;
+    }
+
+
+    else{
+        return 1;
+    }
+
+}
